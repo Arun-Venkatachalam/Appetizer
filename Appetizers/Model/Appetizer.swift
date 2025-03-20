12c496e8c34fb3bb87dc3c6742c118f629306774
Arun V <arunsmart447@gmail.com>
2025-03-20 19:57:42 +0530

    AppetizerDetailView_ Initial - UI design

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+struct Appetizer: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let imageURL: String
+    let price: Double
+    let carbs: Int
+    let protein: Int
+    let calories: Int
+}
+
+struct AppetizerResponse: Decodable {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    
+    static let sampleAppetizer = Appetizer(
+        id: 0001,
+        name: "Test Appetizer",
+        description: "This is the description for my appetizers. It's yummy.",
+        imageURL: "",
+        price: 99,
+        carbs: 99,
+        protein: 99,
+        calories: 99
+    )
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
--- Appetizers/Screens/AppetizerListView/DetailView/DetailView.swift
+++ Appetizers/Screens/AppetizerListView/DetailView/DetailView.swift
@@ -8,11 +8,96 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    var appetizer: Appetizer
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .frame(width: 300, height: 220)
+                .scaledToFit()
+            
+            VStack() {
+                Text(appetizer.name)
+                    .fontWeight(.semibold)
+                    .font(.title)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    //.fontWeight(.ultraLight)
+                    //.fixedSize(horizontal: false, vertical: true)
+                    .padding()
+                    //.lineLimit(2)
+                
+                HStack {
+                    VStack(spacing: 10) {
+                        Text("Calories")
+                            .bold()
+                            .font(.caption)
+                        
+                        Text("\(appetizer.calories)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    .padding()
+                    
+                    VStack(spacing: 10) {
+                        Text("Carbs")
+                            .bold()
+                            .font(.caption)
+                        
+                        Text("\(appetizer.carbs)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    .padding()
+                    
+                    VStack(spacing: 10) {
+                        Text("Protein")
+                            .bold()
+                            .font(.caption)
+                        
+                        Text("\(appetizer.protein)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    .padding()
+                }
+                
+                Button(action: {
+                    print("Add to Card Tapped")
+                }, label: {
+                    Text("$\(appetizer.price, specifier: "%.2f") - Add To Order")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .frame(width: 250, height: 50)
+                        .foregroundStyle(.white)
+                        .background(.brandPrimary)
+                        .clipShape(.buttonBorder)
+                })
+                .padding(.bottom)
+            }
+        }
+        .background(Color(.systemBackground))
+        .frame(width: 300, height: 520)
+        .cornerRadius(12)
+        .shadow(radius: 40)
+        .overlay(alignment: .topTrailing) {
+            Image(systemName: "xmark.circle.fill")
+                .resizable()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.white)
+            
+        }
     }
 }
 
+// Preview
 #Preview {
-    DetailView()
+    DetailView(appetizer: MockData.sampleAppetizer)
 }
