61b32acb63b64f673755423f2bffa5a226c1294d
Arun V <arunsmart447@gmail.com>
2025-03-22 02:08:44 +0530

    Created AccountView - Forms

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var date = Date()
+    @State private var extraNapkin = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationStack {
-            Text("Account View !")
-                .navigationTitle("👨🏻 Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                        .keyboardType(.emailAddress)
+                    
+                    DatePicker("Birthday",
+                               selection: $date,
+                               displayedComponents: .date)
+                    //.foregroundStyle(.brandPrimary)
+                    .tint(.brandPrimary)
+                    
+                    Button {
+                        print("Save Changes button clicked")
+                    } label: {
+                        Text("Save Changes")
+                    }
+  
+                } header: {
+                    Text("Personal Info")
+                }
+                
+                Section {
+                    Toggle("Extra Napkins", isOn: $extraNapkin)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                } header: {
+                    Text("Requests")
+                }
+                .tint(.brandPrimary)
+            }
+            .navigationTitle("👨🏻 Account")
         }
     }
 }
